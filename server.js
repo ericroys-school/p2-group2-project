a3524018fc5009d253de9527fa16520b36005188
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-24 21:39:22 -0500

    update server route references

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { apiRoutes } from "./controllers/api/index.js";
+import { router } from "./controllers/index.js";
 import { dbConnect } from "./config/connection.js";
 import { create } from "express-handlebars";
 import path from "node:path";
@@ -11,13 +11,13 @@ const dir = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(apiRoutes);
+app.use(router);
 app.engine("handlebars", create({}).engine);
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 //serve the start page
-app.get("/", (req, res) => res.sendFile(path.join(dir, "/public/index.html")));
+// app.get("/", (req, res) => res.sendFile(path.join(dir, "/public/index.html")));
 
 // sync sequelize models to the database, then turn on the server
 dbConnect.sync().then(() => {
