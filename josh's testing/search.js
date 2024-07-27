0e408faf14f28df9fcf61497814d12d7b3740e3f
Josh Stringer <joshstringer@live.com>
2024-07-27 16:43:12 -0500

    selection now pulls all associated data from json, also fixed a path for one of the pictures

--- josh's testing/search.js
+++ josh's testing/search.js
@@ -2,34 +2,51 @@
 $( function() {
     $( "#selectState" ).selectmenu();
     
-    fetch('../seeds/area.json')
-        .then(response => response.json())
-        .then(jsonData => {
-            console.log('JSON data:', jsonData);
+        Promise.all([
+            fetch('../seeds/area.json').then(response => response.json()),
+            fetch('../seeds/location.json').then(response => response.json())
+        ]) .then(([areaData, locationData]) => {
+            console.log('Area Data: ', areaData);
+            console.log('Location Data: ', locationData);
 
-        jsonData.forEach(item => {
-            $("#selectState").append(
-                new Option(item.name, item.id)
-            );
-        });
+            const locationDataMap = locationData.reduce((map, item) => {
+                map[item.id] = item;
+                return map;
+            }, {});
+
+            const areaDataMap = areaData.reduce((map, item) => {
+                map[item.location_id] = item;
+                return map;
+            }, {});
+
+            locationData.forEach(item => {
+                $("#selectState").append(
+                    new Option(item.state, item.id)
+                );
+            });
 
         $("#selectState").selectmenu("refresh");
 
             $("#selectState").on("selectmenuchange", function() {
                 var selectedId = $(this).val();
-                console.log("Selected ID: ", selectedId);
+                console.log("Selected Location ID: ", selectedId);
 
-                var selectedData = jsonData.find(item => item.id === selectedId);
-                console.log('Selected Data: ', selectedData);
+                var selectedLocationData = locationDataMap[selectedId];
+                console.log('Selected Location Data: ', selectedLocationData);
+                
+                if (selectedLocationData) {
+                var correspondingAreaData = areaDataMap[selectedLocationData.id];
+                console.log('Corresponding Area Data: ', correspondingAreaData);
 
-                if (selectedData) {
+                if (correspondingAreaData) {
                     $("#areaInfo").html(
-                         `<h2>${selectedData.name}</h2>
-                         <p>Coordinates: ${selectedData.coordinates}</p>
-                         <img src="${selectedData.photo}" alt="${selectedData.name}" style="max-width: 100%; height: auto;">`
+                         `<h1>${correspondingAreaData.name}</h1>
+                         <h3>State: ${selectedLocationData.state}</h3>
+                         <p>Coordinates: ${correspondingAreaData.coordinates}</p>
+                         <img src="${correspondingAreaData.photo}" alt="${correspondingAreaData.name}" style="max-width: 100%; height: auto;">`
                     );
                 };
-            });
+        }});
         }) .catch(error => {
             console.error("error fetching json:", error);
             $("#areaInfo").html("error loading data");
