5d5699568998ef122f4965abf3d4bb047296b829
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-28 13:57:00 -0500

    add difficulty-yds to include
    added difficulty_yds as include when query for a climb by id

--- controllers/api/climb/climb.js
+++ controllers/api/climb/climb.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { Climb } from "../../../models/Climb.js";
+import { Difficulty_YDS } from '../../../models/Difficulty_YDS.js'
 import { Climb_Comment } from '../../../models/Climb_Comment.js'
 import { responseError } from "../../util.js";
 export const climbRouter = Router();
 
 climbRouter.get("/:id", async (req, res) => {
     try {
-        let u = await Climb.findByPk(req.params.id, {include: [Climb_Comment]});
+        let u = await Climb.findByPk(req.params.id, {include: [Climb_Comment, Difficulty_YDS]});
         u ? res.status(200).json(u) : responseNotFound(res, req.params.id);
       } catch (err) {
         responseError(res, err);
