a008a8d7e83ee42af1a3c6d4227b30af52a9f9b1
eric.roys@gmail.com <eric.roys@gmail.com>
2024-07-29 12:09:17 -0500

    make add climb to activity work

---
+++ public/scripts/climb.js
@@ -0,0 +1,36 @@
+import { setError, setMessage } from "./util.js";
+
+$("#toDoButton").on("click", addActivity);
+
+function addActivity(event) {
+  event.preventDefault();
+  let id = $(event.target).attr("data-id");
+
+  if(addToActivity(id)){
+    $("#toDoButton").css('visibility', 'hidden');
+    setMessage("Climb added")
+  }
+}
+
+async function addToActivity(climbId) {
+  try {
+    const res = await fetch(`/api/activity/${climbId}`, {
+      method: "POST",
+      body: JSON.stringify({}),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!res.ok) {
+      if (res.status !== 401) {
+        let m = await res.json();
+        setError(m.message);
+      } else setError("You must be logged in to add a climb activity");
+      return false;
+    } else {
+      console.log("YEAH!!!!");
+      return true;
+    }
+  } catch (err) {
+    setError(err);
+    return false;
+  }
+}
